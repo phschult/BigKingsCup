43d7b5fb6ca06207148a6e02f7df9a276cef21ac
schphil <schphil@STAG1134M.stadlerrail.ch>
2015-04-24 11:10:53 +0200

    Methode statisticCounter improved

--- src/bigkingscup/model/Player.java
+++ src/bigkingscup/model/Player.java
@@ -55,37 +55,48 @@ public class Player {
     public String getGender() {
         return this.gender;
     }
+    /**
+     * Durchsucht die Karte auf ein 'o'. Wenn gefunden true 
+     * und das Wort wird abgeschnitten 
+     * @param card
+     * @param index
+     * @return 
+     */
+    private boolean compare(String card, int index) {
+        return card.charAt(index) == 'o';
+    }
 
     /**
      * Shows which playing cards of the players as often has drawn.
      */
     public void statisticCounter() {
+        //Idee: iteriere durch liste und schneide alles hinter Zahl ab um
+        //gleiche Karten zählen zu können.
         for (int i = 0; i < hand.size(); i++) {
-            //Idee: iteriere durch liste und schneide alles Hinter Zahl ab um
-            //gleiche Karten zählen zu können.
-            if (hand.get(i).charAt(1) == 'o') {
-                //abeschneiden und in extraliste schreiben.
-                number.add(hand.get(i).substring(0,1));
-            } else if (hand.get(i).charAt(3) == 'o') {
-                number.add(hand.get(i).substring(0,3));
-            } else if (hand.get(i).charAt(4) == 'o') {
-                number.add(hand.get(i).substring(0,4));
-            } else if (hand.get(i).charAt(5) == 'o') {
-                number.add(hand.get(i).substring(0,5));
+            //Case Number
+            if (compare(hand.get(i), 1)) {
+                //abeschneiden und in extra Liste number schreiben.
+                number.add(hand.get(i).substring(0, 1));
+                //Case ASS
+            } else if (compare(hand.get(i), 3)) {
+                number.add(hand.get(i).substring(0, 3));
+                //Case KING, JACK
+            } else if (compare(hand.get(i), 4)) {
+                number.add(hand.get(i).substring(0, 4));
+                //Case QUEEN
+            } else if (compare(hand.get(i), 5)) {
+                number.add(hand.get(i).substring(0, 5));
             }
-
-            for (String temp : number) {
-                Integer count = stat.get(temp);
-                stat.put(temp, count + 1);
+            //Vergleiche jede Karte auf der Hand mit den anderen und zähle wie
+            //viele gleiche Karten sich auf der Hand befinden
+            for (int j = 0; j < number.size(); j++) {
+                for (int z = 0; z < number.size(); z++) {
+                    if (number.get(j).equals(number.get(z))) {
+                        countCards++;
+                        stat.put(number.get(i), countCards);
+                    }
+                }
             }
         }
-//        for (int i = 0; i < hand.size(); i++) {
-//            for (int j = 0; j < hand.size(); j++) {
-//                if (hand.get(i).equals(hand.get(j))) {
-//                    countCards++;
-//                    stat.put(hand.get(i), countCards);
-//                }
-//            }
-//        }
     }
 }
