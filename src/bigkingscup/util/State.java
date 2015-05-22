e9ee8b720fee166e83beb10c6ce946fe9a9e5fee
schphil <schphil@STAG1134M.stadlerrail.ch>
2015-05-22 15:13:10 +0200

    Try to add a State Machine

---
+++ src/bigkingscup/util/State.java
@@ -0,0 +1,25 @@
+package bigkingscup.util;
+
+/**
+ *
+ * @author philippschultheiss
+ */
+public enum State {
+   /**
+    * Enum State for state machine
+    */
+    
+    TWO,
+    THREE,
+    FOUR,
+    FIVE,
+    SIX,
+    SEVEN,
+    EIGHT,
+    NINE,
+    TEN,
+    JACK,
+    QUEEN,
+    KING,
+    ACE;
+}
