cdf55a01244b4e06d3ca9cda44066013df859a18
schphil <schphil@STAG1134M.stadlerrail.ch>
2015-06-09 11:56:00 +0200

    State Maschine added and changed to a round based game

--- src/bigkingscup/controller/impl/Controller.java
+++ src/bigkingscup/controller/impl/Controller.java
@@ -1,6 +1,6 @@
 package bigkingscup.controller.impl;
 
-import bigkingscup.controller.IGameState;
+import bigkingscup.controller.State;
 import bigkingscup.model.ICard;
 import bigkingscup.model.IDeck;
 import bigkingscup.model.impl.Deck;
@@ -16,45 +16,63 @@ import java.util.List;
  */
 public class Controller extends Observable {
 
-    private final IDeck deck;
-    private final Ringbuffer rBuffer;
+    private IDeck deck;
+    private Ringbuffer rBuffer;
     private ICard actualCard;
-    private IGameState currentState = null;
-    private String statusFlag;
-    private final List rules = new LinkedList();
+    private State currentState;
+    private String statusMessage = "Welcome to BigKingsCup!";
+    private String ruleFlag;
+    private boolean roundFlag = false;
+    private List rules = new LinkedList();
 
     public Controller() {
         this.deck = new Deck();
         this.rBuffer = new Ringbuffer();
     }
 
-    public void setCurrentState(final IGameState state) {
-        this.currentState = state;
+    public void setCurrentState(State currentState) {
+        this.currentState = currentState;
         notifyObservers();
     }
 
-    public IGameState getCurrentState() {
+    public State getCurrentState() {
         return currentState;
+    }
+
+    public void setRoundFlag(boolean flag) {
+        this.roundFlag = flag;
+    }
+
+    public boolean getRoundFlag() {
+        return roundFlag;
+    }
+
+    public void setStatusMessage(String message) {
+        this.statusMessage = message;
+    }
+
+    public String getStatusMessage() {
+        return statusMessage;
     }
 
     public boolean checkNumOfPlayers() {
         return getBuffer().getSize() == 0;
     }
-    
-    public void checkGameState() {
-        if (this.currentState == null) {
-            this.setCurrentState(new StateInGame(this));
-            this.currentState.change();
-        } else {
-            //check if GameState will change
-            this.currentState.change();
-        }
+
+    public void newRound() {
+        this.deck = new Deck();
+        this.rules = new LinkedList();
+        this.rBuffer = new Ringbuffer();
+        this.actualCard = null;
+        this.currentState = null;
+        this.roundFlag = false;
+        this.statusMessage = "Welcome to a new Round BigKingsCup!";
     }
-    
-    public void addRule (String rule) {
+
+    public void addRule(String rule) {
         this.rules.add(rule);
     }
-    
+
     public void addPlayer(final String name, final String gender) {
         rBuffer.put(new Player(name, gender));
     }
@@ -70,7 +88,7 @@ public class Controller extends Observable {
     public Player getPlayer() {
         return rBuffer.get();
     }
-    
+
     public List getRules() {
         return this.rules;
     }
@@ -80,9 +98,12 @@ public class Controller extends Observable {
     }
 
     public void checkDeckState() {
-        if (this.getDeck().getNumOfCards() == 0) {
-            System.out.println("Game Over. There are no more cards on the Stack");
-            System.exit(1);
+        if (this.getDeck().getNumOfCards() == 0 && getRoundFlag()) {
+            if (getRoundFlag()) {
+                newRound();
+            } else {
+                System.exit(1);
+            }
         }
     }
 
@@ -90,19 +111,7 @@ public class Controller extends Observable {
         this.actualCard = card;
         notifyObservers();
     }
-    
-    //if status then... 
-    //Idee: Methode die je nach Status reagiert .. doTask() ersetzen
-    //Idee: Methode die festhält wer momentan was ist.. daumenkönig, questionmaster
-    public String getStatusFlag() {
-        return this.statusFlag;
-    }
-    
-    private void setStatusFlag(String flag) {
-        this.statusFlag = flag;
-        notifyObservers();
-    } 
-   
+
     public String printPlayersHand() {
         StringBuilder sb = new StringBuilder();
         for (int i = 0; i < rBuffer.getSize(); i++) {
@@ -110,7 +119,7 @@ public class Controller extends Observable {
         }
         return sb.toString();
     }
-    
+
     public void printHelpMenue() {
         System.out.println("---------------- HELP ----------------");
         System.out.println("d - deal card\n" + "n - number of remaining cards\n"
@@ -118,67 +127,87 @@ public class Controller extends Observable {
                 + "h - display players\n" + "r - rules\n" + "q - quit game\n");
     }
 
-    public void doTask(ICard card) {
+    public void change(ICard card) {
         String value = card.toString();
 
         String[] parts = value.split("Of");
         String temp = parts[0];
 
-        switch (temp) {
-            case "Two":
-                System.out.println("Zwei Schlücke verteilen/selber trinken");
-                setStatusFlag("Zwei Schlücke verteilen/selber trinken");
-                break;
-            case "Three":
-                System.out.println("Drei Schlücke verteilen/selber trinken");
-                setStatusFlag("Drei Schlücke verteilen/selber trinken");
-                break;
-            case "Four":
-                System.out.println("Questionmaster");
-                setStatusFlag("Questionmaster");
-                break;
-            case "Five":
-                System.out.println("Regel");
-                setStatusFlag("Regel");
-                break;
-            case "Six":
-                System.out.println("Ich habe noch nie...");
-                setStatusFlag("Ich habe noch nie...");
-                break;
-            case "Seven":
-                System.out.println("Schlücke verteilen");
-                setStatusFlag("Schlücke verteilen");
-                break;
-            case "Eight":
-                System.out.println("King Thumb");
-                setStatusFlag("King Thumb");
-                break;
-            case "Nine":
-                System.out.println("Mädchen");
-                setStatusFlag("Mädchen");
-                break;
-            case "Ten":
-                System.out.println("Jungs");
-                setStatusFlag("Jungs");
-                break;
-            case "Jack":
-                System.out.println("Augenkontakt");
-                setStatusFlag("Augenkontakt");
-                break;
-            case "Queen":
-                System.out.println("Kategorie");
-                setStatusFlag("Kategorie");
-                break;
-            case "King":
-                System.out.println("Reim");
-                setStatusFlag("Reim");
-                break;
-            case "Ace":
-                System.out.println("Alle müssen trinken");
-                setStatusFlag("Alle müssen trinken");
-                break;
-            default:
+        if (temp.equals("Two")) {
+            setCurrentState(State.TWO);
+            setStatusMessage("Zwei Schlücke verteilen/selber trinken");
+        } else if (temp.equals("Three")) {
+            setCurrentState(State.THREE);
+            setStatusMessage("Drei Schlücke verteilen/selber trinken");
+        } else if (temp.equals("Four")) {
+            setCurrentState(State.FOUR);
+            setStatusMessage("Questionmaster");
+        } else if (temp.equals("Five")) {
+            setCurrentState(State.FIVE);
+            setStatusMessage("Regel");
+        } else if (temp.equals("Six")) {
+            setCurrentState(State.SIX);
+            setStatusMessage("Ich habe noch nie...");
+        } else if (temp.equals("Seven")) {
+            setCurrentState(State.SEVEN);
+            setStatusMessage("Schlücke verteilen");
+        } else if (temp.equals("Eight")) {
+            setCurrentState(State.EIGHT);
+            setStatusMessage("King Thumb");
+        } else if (temp.equals("Nine")) {
+            setCurrentState(State.NINE);
+            setStatusMessage("Mädchen");
+        } else if (temp.equals("Ten")) {
+            setCurrentState(State.TEN);
+            setStatusMessage("Jungs");
+        } else if (temp.equals("Jack")) {
+            setCurrentState(State.JACK);
+            setStatusMessage("Augenkontakt");
+        } else if (temp.equals("Queen")) {
+            setCurrentState(State.QUEEN);
+            setStatusMessage("Kategorie");
+        } else if (temp.equals("King")) {
+            setCurrentState(State.KING);
+            setStatusMessage("Reim");
+        } else if (temp.equals("Ace")) {
+            setCurrentState(State.ACE);
+            setStatusMessage("Alle müssen trinken");
         }
     }
 
+    public String checkGameState() {
+        change(actualCard);
+        String temp = null;
+        if (getCurrentState() == State.TWO) {
+            temp = getStatusMessage();
+        } else if (getCurrentState() == State.THREE) {
+            temp = getStatusMessage();
+        } else if (getCurrentState() == State.FOUR) {
+            temp = getStatusMessage();
+        } else if (getCurrentState() == State.FIVE) {
+            temp = getStatusMessage();
+            System.out.print("\n--> ");
+            ruleFlag = scanner.next();
+            addRule(ruleFlag);
+        } else if (getCurrentState() == State.SIX) {
+            temp = getStatusMessage();
+        } else if (getCurrentState() == State.SEVEN) {
+            temp = getStatusMessage();
+        } else if (getCurrentState() == State.EIGHT) {
+            temp = getStatusMessage();
+        } else if (getCurrentState() == State.NINE) {
+            temp = getStatusMessage();
+        } else if (getCurrentState() == State.TEN) {
+            temp = getStatusMessage();
+        } else if (getCurrentState() == State.JACK) {
+            temp = getStatusMessage();
+        } else if (getCurrentState() == State.QUEEN) {
+            temp = getStatusMessage();
+        } else if (getCurrentState() == State.JACK) {
+            temp = getStatusMessage();
+        } else if (getCurrentState() == State.ACE) {
+            temp = getStatusMessage();
+        }
+        return temp;
+    }
 }
